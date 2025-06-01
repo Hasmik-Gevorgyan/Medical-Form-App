5d488ed55f9aa2ef76a9f7a0ce2c13d84e900e33
Hasmik-Gevorgyan <gevorgyanhasmik10@gmail.com>
2025-06-01 16:32:36 +0400

    Fix: auth service fixes.

--- src/App.tsx
+++ src/App.tsx
@@ -10,15 +10,10 @@ import { getHospitals } from '@/features/hospitalsSlice';
 import { fetchUser } from '@/features/authSlice';
 import { router } from "@/routes";
 import '@/App.css'
-import { renderStatus } from "@/utils/checkStateStatus";
-import { Status } from "@/constants/enums";
 
 function App() {
   const dispatch = useDispatch<AppDispatch>();
-  const [firstLoad, setFirstLoad] = useState(true);
   const mode = useSelector((state: RootState) => state.theme.mode);
-  const {status} = useSelector((state: RootState) => state.auth);
-  const stateStatus = renderStatus(status || Status.LOADING, null);
   
   const antdThemeAlgorithm = mode === 'dark' ? theme.darkAlgorithm : theme.defaultAlgorithm;
 
@@ -31,11 +26,8 @@ function App() {
   }, []);
 
   useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (user) => {
-      if (user) {
-        dispatch(fetchUser(user?.uid));
-      }
-      setFirstLoad(false);
+    const unsubscribe = onAuthStateChanged(auth, async (user) => {
+      dispatch(fetchUser(user?.uid));
     });
 
     return () => unsubscribe();
@@ -45,10 +37,6 @@ function App() {
     document.documentElement.classList.remove('light', 'dark');
     document.documentElement.classList.add(mode);
   }, [mode]);
-
-  if (stateStatus || firstLoad) {
-    return stateStatus;
-  }
   
   return (
     <ConfigProvider theme={{ algorithm: antdThemeAlgorithm }}>
